98f98b6569789d0b7d7e77ae73e00abaea6a85cf
Guilherme Laux <guilherme.laux@compliancetotal.com.br>
2022-02-08 00:36:07 -0300

    cria componente presente

---
+++ components/Presente.jsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Presente.module.css'
+export default function Presente(params) {
+    return (
+        <>
+            <div className= {styles.presente}>
+                <div className={styles.tampa}></div>
+                <div className={styles.corpo}></div>
+                <div className={styles.laco1}></div>
+                <div className={styles.laco2}></div>
+
+
+            </div>
+
+
+        </>
+    )
+
+};
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Presente from '../components/Presente'
+export default function Home() {
+  return (
+    <div >
+
+
+      <Presente></Presente>
+    </div>
+  )
+}
